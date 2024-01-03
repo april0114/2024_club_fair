c2bd90ccf9724bc6d6977f9d59c615c9a4dcc273
april <sue5520607@skuniv.ac.kr>
2024-01-03 22:54:20 +0900

    두번째 날

---
+++ js/data.js
@@ -0,0 +1,13 @@
+const qnaList = [
+    {
+        q: '1. 1번 질문 어쩌고',
+        a:  [
+            {answer : ' a. 선지 1번', type : ['a','b','c','d','e','f']},
+            {answer : ' b. 선지 2번', type : ['a','b','c','d','e','f']},
+            {answer : ' c. 선지 3번', type : ['a','b','c','d','e','f']},
+            {answer : ' d. 선지 4번', type : ['a','b','c','d','e','f']},
+
+        ]
+
+    }
+]
