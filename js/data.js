95ca34e565f645c7c652524d19a1c1debfef1eed
april <sue5520607@skuniv.ac.kr>
2024-02-19 09:20:20 +0900

    진행바 수정

--- js/data.js
+++ js/data.js
@@ -1,10 +1,16 @@
 const qnaList = [
     {
-        q: '1. 1번 질문 어쩌고',
+        //perform = 공연 예술 분과
+        //create = 전시 창작과
+        //hobby = 교양분과
+        // pe = 체육분과
+        // religion = 종교 분과
+        // study = 학술 분과
+        q: '1. 동아리 박람회에 온 나 뭐부터 할까?',
         a:  [
-            {answer : ' a. 선지 1번', type : ['a','b','c','d','e','f']},
-            {answer : ' b. 선지 2번', type : ['a','b','c','d','e','f']},
-            {answer : ' c. 선지 3번', type : ['a','b','c','d','e','f']},
+            {answer : ' a.  말해 뭐해, 구경할래! 공연볼래!', type : ['perform','create']},
+            {answer : ' b. 나는 노는 시간도 아까워! 나한테 도움될만할 동아리 찾아볼래! ', type : ['study','hobby']},
+            {answer : ' c. 부스체험이나 해볼까?', type : ['pe']},
 
         ]
 
