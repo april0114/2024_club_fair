372bd515ea904fbbed7a7c5976f0e9cdf21b104e
april0114 <sue5520607@skuniv.ac.kr>
2024-03-26 10:43:27 +0900

    수정

--- js/share.js
+++ js/share.js
@@ -8,7 +8,15 @@ window.onload = function() {
     "img/question/question-5.jpg",
     "img/question/question-6.jpg",
     "img/question/question-7.jpg",
-    // 나머지 이미지 경로들도 추가해주세요.
+    "img/question/question-7.jpg",
+    "img/result/image-0.jpg",
+    "img/result/image-1.jpg",
+    "img/result/image-2.jpg",
+    "img/result/image-3.jpg",
+    "img/result/image-4.jpg",
+    "img/result/image-5.jpg",
+
+
   ];
 
   function preloadImages(imageURLs) {
